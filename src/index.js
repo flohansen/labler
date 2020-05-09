31198923f369ad182818d3ca653ba66c17b0ef5c
Kluddizz <flhansen.93@gmail.com>
2020-05-09 22:39:33 +0200

    Add redirection if not logged in

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Redirect, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import LoginPage from './_components/LoginPage';
 import * as serviceWorker from './serviceWorker';
 
+const loggedIn = false;
+
 ReactDOM.render(
   <React.StrictMode>
 		<Router>
 			<Switch>
+
+				{ !loggedIn ? (
+						<Redirect from="/app" to="login" />
+					) : (
+						null
+					)
+				}
+
 				<Route path="/app">
 					<App />
 				</Route>
