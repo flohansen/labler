c2bd5897068bcbe1792ce35c3e881ed6027d7a3a
Kluddizz <flhansen.93@gmail.com>
2020-05-12 01:58:45 +0200

    Add auth context class

---
+++ src/_contexts/AuthContext.js
@@ -0,0 +1,33 @@
+import React, { useEffect, createContext } from "react";
+
+const AuthContext = createContext();
+
+const useStateLocalStorage = key => {
+
+	const [value, setValue] = useState(
+		localStorage.getItem(key);
+	);
+
+	useEffect(() => {
+		localStorage.setItem(key, value);
+	}, [key, value]);
+
+	return [value, setValue];
+};
+
+const AuthProvider = ({ ...props }) => {
+
+	const [token, setToken] = useStateLocalStorage('auth_id');
+
+	return (
+		<AuthContext.Provider value={token}>
+			{props.children}
+		</AuthContext.Provider>
+	);
+
+};
+
+export {
+	AuthContext as default,
+	AuthProvider
+}
