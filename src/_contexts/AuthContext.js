226013bd309a340a99af84a5545a6a75db2ebbc7
Kluddizz <flhansen.93@gmail.com>
2020-05-24 14:23:05 +0200

    Fixed bug when no token is set

--- src/_contexts/AuthContext.js
+++ src/_contexts/AuthContext.js
@@ -3,10 +3,7 @@ import React, { useState, useEffect, createContext } from "react";
 const AuthContext = createContext();
 
 const useStateLocalStorage = key => {
-
-	const [value, setValue] = useState(
-		localStorage.getItem(key)
-	);
+	const [value, setValue] = useState(localStorage.getItem(key));
 
 	useEffect(() => {
 		localStorage.setItem(key, value);
@@ -16,28 +13,27 @@ const useStateLocalStorage = key => {
 };
 
 const AuthProvider = ({ ...props }) => {
-
-	const [token, setToken] = useStateLocalStorage('auth_id');
+	const [token, setToken] = useStateLocalStorage("auth_id");
 	const [payload, setPayload] = useState();
 
 	useEffect(() => {
-		const parts = token.split('.');
+		if (token !== "null") {
+			const parts = token.split(".");
 
-		if (parts.length === 3) {
-			const payload = JSON.parse(atob(parts[1]));
-			setPayload(payload);
+			if (parts.length === 3) {
+				const payload = JSON.parse(atob(parts[1]));
+				setPayload(payload);
+			}
 		}
 	}, [token]);
 
 	return (
-		<AuthContext.Provider value={{ token: [token, setToken], payload: [payload, setPayload] }}>
+		<AuthContext.Provider
+			value={{ token: [token, setToken], payload: [payload, setPayload] }}
+		>
 			{props.children}
 		</AuthContext.Provider>
 	);
-
 };
 
-export {
-	AuthContext as default,
-	AuthProvider
-}
+export { AuthContext as default, AuthProvider };
