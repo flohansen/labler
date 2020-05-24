1304955b5d0244157e33ac78b257ef011e587617
Kluddizz <flhansen.93@gmail.com>
2020-05-25 01:13:55 +0200

    Change theme colors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Redirect, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import LoginPage from './_components/LoginPage';
-import PrivateRoute from './_components/PrivateRoute';
-import { AuthProvider } from './_contexts/AuthContext';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+	Redirect,
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import LoginPage from "./_components/LoginPage";
+import PrivateRoute from "./_components/PrivateRoute";
+import { AuthProvider } from "./_contexts/AuthContext";
+import * as serviceWorker from "./serviceWorker";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: blue
+	}
+});
 
 ReactDOM.render(
-	<AuthProvider>
-		<Router>
-			<Switch>
-				<PrivateRoute path="/app" component={App} />
+	<ThemeProvider theme={theme}>
+		<AuthProvider>
+			<Router>
+				<Switch>
+					<PrivateRoute path="/app" component={App} />
 
-				<Route path="/login">
-					<LoginPage />
-				</Route>
+					<Route path="/login">
+						<LoginPage />
+					</Route>
 
-				<Redirect from="/" to="app" />
-			</Switch>
-		</Router>
-	</AuthProvider>,
-  document.getElementById('root')
+					<Redirect from="/" to="app" />
+				</Switch>
+			</Router>
+		</AuthProvider>
+	</ThemeProvider>,
+	document.getElementById("root")
 );
 
 serviceWorker.unregister();
